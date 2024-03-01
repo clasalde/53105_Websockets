4706040f0e05155965eb88195e48c5de2af03cb9
clasalde <calasalde@gmail.com>
2024-03-01 10:57:52 -0300

    Con Multer para múltiples archivos funcionando!

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import multer from "multer";
 
 const app = express();
 const PUERTO = 8080;
@@ -10,6 +11,25 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+
+
+//Upload images with multer - START
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/img");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.array("kamado"), (req, res) => {
+    res.send("Image uploaded!")
+})
+//Upload images with multer - END
+
 
 app.listen(PUERTO, () => {
     console.log(`Server running on port: ${PUERTO}`);
